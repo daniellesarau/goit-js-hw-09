ced0e315d6daeb25d5b19bc5c3220e497bf913b1
daniellesarau <123309317+daniellesarau@users.noreply.github.com>
2023-06-25 12:06:38 +0300

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let timerId = null;
 
+startButton.addEventListener('click', changeColor);
+stopButton.addEventListener('click', stopColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeColor() {
+  startButton.disabled = true;
+  stopButton.disabled = false;
+  timerId = setInterval(randomColor, 1000);
+}
+
+function stopColor() {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  clearInterval(timerId);
+}
+
+function randomColor() {
+  const colorValue = getRandomHexColor();
+  document.body.style.backgroundColor = colorValue;
+}
